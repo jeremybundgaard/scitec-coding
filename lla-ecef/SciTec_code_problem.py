f989bbfb1b413b283793cdbaee72148800edc1c2
Jeremy Bundgaard <jeremybundgaard@me.com>
2020-11-10 13:51:57 -0700

    complete

--- lla-ecef/SciTec_code_problem.py
+++ lla-ecef/SciTec_code_problem.py
@@ -8,16 +8,25 @@ import funcs
 print ('\n')
 print("SciTec_code_problem.py \n")
 
-
+#####################################################################
 # Read the CSV file
+#####################################################################
+
+print("Read the CSV file: data/SciTec_code_problem_data.csv ... ")
+
 csv_in="data/SciTec_code_problem_data.csv"
 lla = np.genfromtxt(csv_in, delimiter=',')
-print("read data/SciTec_code_problem_data.csv -> time, lat, lon, alt\n")
+
+print("DONE\n")
+#####################################################################
+# Convert LLA to ECEF
+#####################################################################
+
+print("Converting input LLA timeseries to ECEF ... ")
 
 # create empty array for ECEF values
 ecef = np.empty(np.shape(lla), float)
 
-# Convert LLA to ECEF
 for i in range(len(lla)):
 	position_ecef = funcs.lla2ecef(lla[i,1],lla[i,2],lla[i,3])
 	t = lla[i,0]
@@ -28,58 +37,76 @@ for i in range(len(lla)):
 	np.set_printoptions(precision=30, suppress=True)
 	# print(ecef[i,:])
 
-print('\n')
-
-# make some plots to see these data
-fig2 = plt.figure(2)
-# fig.set_size_inches(10,10)
-ax3d = fig2.add_subplot(111, projection='3d')
-ax3d.set_xlim3d(min(ecef[1:10,1]),max(ecef[1:10,1]))
-ax3d.set_ylim3d(min(ecef[1:10,2]),max(ecef[1:10,2]))
-ax3d.set_zlim3d(min(ecef[1:10,3]),max(ecef[1:10,3]))
-ax3d.set_xlabel('x', fontsize=30)
-ax3d.set_ylabel('y', fontsize=30)
-ax3d.set_zlabel('z', fontsize=30)
-t = ecef[:,0]
-x = ecef[:,1]
-y = ecef[:,2]
-z = ecef[:,3]
-
-from scipy.interpolate import splprep, splev
-tck, u = splprep([x, y, z], s=0)
-spline = splev(u, tck)
-
-ax3d.scatter3D(spline[0],spline[1],spline[2], 'g-');
-ax3d.scatter3D(x, y, z, 'r');
-fig2.show()
-plt.show()
-
-# ax.scatter3D(x, y, z, c=t, cmap="RdYlGn_r");
-
-########## rotate the axes and update ########## 
-# for angle in range(0, 360,10):
-#     ax.view_init(10, angle)
-#     plt.draw()
-#     plotname = 'ecef_angle'+str(angle)+'.png'
-#     fig.savefig(plotname, dpi=100)
-#     print('saved: ',plotname)
-
+print("DONE\n")
 
 #####################################################################
 # Calculate ECEF velocity at the time points given in the input file
 #####################################################################
-ecef_vel = funcs.ecef_velocity_data(ecef)
+print("Calculating ECEF velocity at the time points given in the input file ... ")
+
+# create empty velocity array for loop
+ecef_velocity = np.zeros(np.shape(ecef))
+
+for i in range(1,len(ecef)):
+	Vx = (ecef[i,1]-ecef[i-1,1])/(ecef[i,0]-ecef[i-1,0])
+	Vy = (ecef[i,2]-ecef[i-1,2])/(ecef[i,0]-ecef[i-1,0])
+	Vz = (ecef[i,3]-ecef[i-1,3])/(ecef[i,0]-ecef[i-1,0])
+	ecef_velocity[i] = np.array([[ecef[i,0]],[Vx],[Vy],[Vz]]).T
+	# print(ecef_velocity[i,:],'\n')
+
+print("DONE\n")
+
+#####################################################################
+# Interpolate ECEF velocity for any requested time
+#####################################################################
+print("Interpolating ECEF velocity for the requested time: 1532333000")
+funcs.interpolateVelocity(ecef_velocity, 1532333000)
+
+print("Interpolating ECEF velocity for the requested time: 1532335268")
+funcs.interpolateVelocity(ecef_velocity, 1532335268)
+
+
+#####################################################################
+# Interpolate ECEF velocity for any requested time
+#####################################################################
+
+# # makes some plots to see these data
+# fig = plt.figure(2)
+# fig.set_size_inches(10,10)
+# ax3d = fig.add_subplot(111, projection='3d')
+# ax3d.set_xlim3d(min(ecef[:,1]),max(ecef[:,1]))
+# ax3d.set_ylim3d(min(ecef[:,2]),max(ecef[:,2]))
+# ax3d.set_zlim3d(min(ecef[:,3]),max(ecef[:,3]))
+# ax3d.set_xlabel('x', fontsize=30)
+# ax3d.set_ylabel('y', fontsize=30)
+# ax3d.set_zlabel('z', fontsize=30)
 
 # t = ecef[:,0]
 # x = ecef[:,1]
 # y = ecef[:,2]
 # z = ecef[:,3]
-# ax.scatter3D(x, y, z);
-# fig.set_size_inches(20,20)
 
-# Interpolate ECEF velocity for any requested time
+# for i in range(len(ecef)):
+# 	if i%25 != 0:
+# 		print()
+# 		t[i] = 0
+# 		x[i] = 0
+# 		y[i] = 0
+# 		z[i] = 0
+
+# ######### rotate the axes and update ########## 
+# for angle in range(0, 360,10):
+# 	ax3d.view_init(10, angle)
+# 	plotname='ecef_angle'+str(angle)+'.png'
+# 	ax3d.plot(spline[0],spline[1],spline[2], 'g-')
+# 	ax3d.scatter3D(x, y, z, 'r*')
+# 	# plt.show()
+# 	fig.savefig(plotname, dpi=100)
+# 	print('saved: ',plotname)
+# 	fig.canvas.flush_events()
+    
+
+print("SciTec Coding Problem Completed \n")
 
 
-
-print ('\n')
-# exit
+exit
--- lla-ecef/funcs.py
+++ lla-ecef/funcs.py
@@ -1,7 +1,10 @@
-
 import numpy as np
 
-# 	coordinate transformations on the WGS-84 spheriod: [lat, lon, alt] -> [x, y, z] in ECEF
+#####################################################################
+####    coordinate transformations on the WGS-84 spheriod:
+####    [lat, lon, alt] -> [x, y, z] in ECEF
+#####################################################################
+
 def lla2ecef(lat, lon, alt):
 
 	f = 1/298.257223563
@@ -22,25 +25,77 @@ def lla2ecef(lat, lon, alt):
 	return [x, y, z]
 
 
-# coordinate transformations on the WGS-84 spheriod: [lat, lon, alt] -> [x, y, z] in ECEF
-def ecef_velocity_data(ecef):
+#####################################################################
+####    velocity interpolation 
+#####################################################################
 
-	t = ecef[:,0]
-	x = ecef[:,1]
-	y = ecef[:,2]
-	z = ecef[:,3]
+def interpolateVelocity(input_velocity, time):
 
-	# create empty velocity array for loop
-	ecef_velocity = np.zeros(np.shape(ecef))
+	# redefine some local variables for readability 
+	Vt = input_velocity[:,0]
+	Vx = input_velocity[:,1]
+	Vy = input_velocity[:,2]
+	Vz = input_velocity[:,3]
 
-	for i in range(1,len(ecef)):
-		# print(i)
-		Vx = (x[i]-x[i-1])/(t[i]-t[i-1])
-		Vy = (y[i]-y[i-1])/(t[i]-t[i-1])
-		Vz = (z[i]-z[i-1])/(t[i]-t[i-1])
-		# print("Vx,Vy,Vz:",Vx,Vy,Vz)
-		# print(np.array([[t[i]],[Vx],[Vy],[Vz]]).T)
-		ecef_velocity[i] = np.array([[t[i]],[Vx],[Vy],[Vz]]).T
-		# print(ecef_velocity[i,:])
+	# Closeset Time Index
+	iCls = np.abs(Vt-time).argmin()
 
-	return ecef_velocity
+	# determin Vt array indeces which contain the input time 
+	if np.abs(Vt[iCls-1]) < np.abs(time) and np.abs(time) < np.abs(Vt[iCls]) :
+		iUpper = iCls
+		iLower = iCls-1
+	elif np.abs(Vt[iCls]) < np.abs(time) and np.abs(time) < np.abs(Vt[iCls+1]) :
+		iUpper = iCls+1
+		iLower = iCls
+
+	# print("\n")
+	# print("upper Bound   Vt[",iUpper,"]: ",Vt[iUpper],'\n')
+	# print("time to be interpolated: ",time,'\n')
+	# print("lower Bound   Vt[",iLower,"]: ",Vt[iLower],'\n')
+	# print("\n")
+
+	time_bounds = [ Vt[iLower], Vt[iUpper] ]
+	Vx_bounds   = [ Vx[iLower], Vx[iUpper] ]
+	Vy_bounds   = [ Vy[iLower], Vy[iUpper] ]
+	Vz_bounds   = [ Vz[iLower], Vz[iUpper] ]
+
+	Vx_interp = np.interp(time, time_bounds, Vx_bounds)
+	Vy_interp = np.interp(time, time_bounds, Vy_bounds)
+	Vz_interp = np.interp(time, time_bounds, Vz_bounds)
+	
+	# graphical validation
+	import matplotlib.pyplot as plt
+	import matplotlib.ticker as mtick
+	fig, ax = plt.subplots()
+	plt.subplots_adjust(left=0.15, bottom=0.15, right=0.95, top=0.9)
+	plt.ylabel('Velocity')
+	plt.xlabel('time')
+	plt.ticklabel_format(axis="x", style="sci", scilimits=(0,0))
+	ax.set_xticks([time_bounds[0],time,time_bounds[1]])
+	ax.set_xticklabels([time_bounds[0],time,time_bounds[1]])
+	plt.xticks(rotation=-10)
+	plt.title('velocity interpolation at time:'+str(time))
+
+	ax.plot(time_bounds, Vx_bounds, 'ro--', linewidth=1, markersize=12)
+	ax.plot(time_bounds, Vy_bounds, 'go--', linewidth=1, markersize=12)
+	ax.plot(time_bounds, Vz_bounds, 'bo--', linewidth=1, markersize=12)
+	ax.plot(time, Vx_interp, 'yo', linewidth=2, markersize=12)
+	plt.gca().legend(('Vx_bounds','Vy_bounds','Vz_bounds','V_interp'))
+	ax.plot(time, Vy_interp, 'yo', linewidth=2, markersize=12)
+	ax.plot(time, Vz_interp, 'yo', linewidth=2, markersize=12)
+	plotname='Vxyz_interpolation_time'+str(time)+'.png'
+	fig.savefig(plotname, dpi=100)
+
+	Vxyz_interp_out = [time, Vx_interp ,Vy_interp, Vz_interp]
+
+	
+	print("     time_bounds      : ",time_bounds)
+	print("     Vx_bounds        : ",Vx_bounds)
+	print("     Vy_bounds        : ",Vy_bounds)
+	print("     Vz_bounds        : ",Vz_bounds)
+	print("     Vxyz_interp_out  : ",Vxyz_interp_out,'\n')
+	print("     See",plotname,"for a graphical interpolation validation")
+
+	print('\n')
+
+	return Vxyz_interp_out
